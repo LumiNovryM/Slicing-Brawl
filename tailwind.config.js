01cb2206c4edf9520c6d0038e6b4d89ac2ca68f5
LumiNovryM <luminovrymekel@gmail.com>
2023-12-05 16:54:08 +0700

    feat: header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    backgroundColor: {
+      'white': '#ffffff',
+      'soft-white': '#f0f3ff',
+      'soft-red': '#fff0e5',
+      'yellow': '#facb3b',
+      'dark-purple': '#090e23',
+      'light-purple': '#3a10e5'
+    },
+    textColor: {
+      'light-purple': '#3a10e5',
+      'white': '#ffffff',
+      'black': '#121313',
+      'gray': '#797c87'
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 }
